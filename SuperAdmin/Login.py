400e12833fe8b03caf622006b28589b8540e7ffb
pixtelation <abhishekmehta50@gmail.com>
2023-12-11 15:39:31 +0530

    Login of Superadmin

---
+++ SuperAdmin/Login.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service #---- It is imported for automatic download of driver 
+from selenium.webdriver.chrome.options import Options  #------ Options are imported to use it to stop automaticallly closing driver 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+
+
+
+
+driver_service = Service(executable_path=ChromeDriverManager().install()) #---- Auto installs webdriver, no need to give path.
+
+options = webdriver.ChromeOptions() 
+
+options.add_experimental_option("detach", True) #-------- It is used in order to stop browser being closed automatically.
+
+driver = webdriver.Chrome(service=driver_service, options=options) 
+
+
+driver.get("https://dev.groundmetrx.com/masterAdmin/login")
+
+driver.find_element(By.XPATH, "//input[@id='email']").send_keys("rpr5@getnada.com")
+driver.find_element(By.XPATH, "//input[@id='password']").send_keys("gm@123")
+driver.find_element(By.XPATH, "//button[normalize-space()='Sign in']").click()
+
